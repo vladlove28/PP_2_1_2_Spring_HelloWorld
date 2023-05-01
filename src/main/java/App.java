5366495e5e611151f53fdec3d05a676f2be815e8
vladlove28 <sergcechisto@gmail.com>
2023-05-01 22:52:36 +0300

    fix pom

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,15 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        Cat cat = (Cat) applicationContext.getBean("Cat");
+        Cat cat2 = (Cat) applicationContext.getBean("Cat");
+        boolean beanHelloWorld = bean.equals(bean2);
+        boolean beanCat = cat.equals(cat2);
+        System.out.println(beanHelloWorld);
+        System.out.println(beanCat);
+        System.out.println(cat.getMessage());
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,9 +1,18 @@
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
+    @Bean(name = "Cat")
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//    должен создаваться новым при каждом запросе
+    public Cat getCat(){
+        Cat cat = new Cat();
+        cat.setMessage("Cat");
+        return cat;
+    }
  
     @Bean(name="helloworld")
     public HelloWorld getHelloWorld() {
